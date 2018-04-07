5d4ebe405aa53b68d6a73f17c8bd17c482d125c8
Daniel Palenicek <daniel.palenicek@gmail.com>
2018-04-07 12:22:58 +0200

    clean up

--- rl/pre_processing.py
+++ rl/pre_processing.py
@@ -31,15 +31,6 @@ for name, arg_type in actions.TYPES._asdict().items():
     is_spatial_action[arg_type] = name in ['minimap', 'screen', 'screen2']
 
 
-def stack_ndarray_dicts(lst, axis=0):
-    """Concatenate ndarray values from list of dicts
-    along new axis."""
-    res = {}
-    for k in lst[0].keys():
-        res[k] = np.stack([d[k] for d in lst], axis=axis)
-    return res
-
-
 class Preprocessor():
     """Compute network inputs from pysc2 observations.
 
@@ -48,41 +39,53 @@ class Preprocessor():
     """
 
     def __init__(self, obs_spec):
-        self.screen_channels = len(features.SCREEN_FEATURES)
-        self.minimap_channels = len(features.MINIMAP_FEATURES)
-        self.flat_channels = len(FLAT_FEATURES)
-        self.available_actions_channels = NUM_FUNCTIONS
+        screen_channels  = len(features.SCREEN_FEATURES)
+        minimap_channels = len(features.MINIMAP_FEATURES)
+        flat_channels    = len(FLAT_FEATURES)
+        available_actions_channels = NUM_FUNCTIONS
 
-    def get_input_channels(self):
-        """Get static channel dimensions of network inputs."""
-        return {
-            'screen': self.screen_channels,
-            'minimap': self.minimap_channels,
-            'flat': self.flat_channels,
-            'available_actions': self.available_actions_channels}
+        def get_input_channels(self):
+            """Get static channel dimensions of network inputs."""
+            return {
+                'screen' : screen_channels,
+                'minimap': minimap_channels,
+                'flat'   : flat_channels,
+                'available_actions': available_actions_channels
+            }
 
-    def preprocess_obs(self, obs_list):
-        return stack_ndarray_dicts(
-            [self._preprocess_obs(o.observation) for o in obs_list])
+        def preprocess_obs(self, obs_list):
+            return _stack_ndarray_dicts([_preprocess_obs(o.observation) for o in obs_list])
 
-    def _preprocess_obs(self, obs):
-        """Compute screen, minimap and flat network inputs from raw observations.
-        """
-        available_actions = np.zeros(NUM_FUNCTIONS, dtype=np.float32)
-        available_actions[obs['available_actions']] = 1
+        def _stack_ndarray_dicts(lst, axis=0):
+            """Concatenate ndarray values from list of dicts
+            along new axis."""
+            res = {}
+            for k in lst[0].keys():
+                res[k] = np.stack([d[k] for d in lst], axis=axis)
+            return res
 
-        screen = self._preprocess_spatial(obs['screen'])
-        minimap = self._preprocess_spatial(obs['minimap'])
+        def _preprocess_spatial(self, spatial):
+            return np.transpose(spatial, [1, 2, 0])
 
-        flat = np.concatenate([
-            obs['player']])
-        # TODO available_actions, control groups, cargo, multi select, build queue
+        def _preprocess_obs(self, obs):
+            """
+            Compute screen, minimap and flat network inputs from raw observations.
+            """
+            available_actions = np.zeros(NUM_FUNCTIONS, dtype=np.float32)
+            available_actions[obs['available_actions']] = 1
 
-        return {
-            'screen': screen,
-            'minimap': minimap,
-            'flat': flat,
-            'available_actions': available_actions}
+            screen  = _preprocess_spatial(obs['screen'])
+            minimap = _preprocess_spatial(obs['minimap'])
 
-    def _preprocess_spatial(self, spatial):
-        return np.transpose(spatial, [1, 2, 0])
+            flat = np.concatenate([obs['player']])
+            # TODO available_actions, control groups, cargo, multi select, build queue
+
+            return {
+                'screen': screen,
+                'minimap': minimap,
+                'flat': flat,
+                'available_actions': available_actions
+            }
+
+        self.get_input_channels = get_input_channels
+        self.preprocess_obs = preprocess_obs

17187655f581045e658475da5ef532932ec4256c
marcelhussing <marcel.hussing@gmail.com>
2018-08-28 14:08:07 +0200

    bugfix

--- rl/agents/feudal/agent.py
+++ rl/agents/feudal/agent.py
@@ -99,10 +99,9 @@ class FeudalAgent():
         # - manager loss
         num = tf.reduce_sum(tf.multiply(S_DIFF,train_model.goal),axis=1)
 
-        #extra check to make sure s_diff norm is not 0
         s_norm = tf.norm(S_DIFF,axis=1)
-        s_norm = tf.where(tf.equal(s_norm, 0), 1e-12, s_norm)
-        
+        s_norm = tf.cond(tf.equal(s_norm, 0), tf.ones_like(denominator) * 1e-12, s_norm)
+
         den = s_norm*tf.norm(train_model.goal,axis=1)
         cos_similarity = safe_div(num, den, "manager_cos")
         manager_loss = -tf.reduce_mean(ADV_M * cos_similarity)
